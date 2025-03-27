475db03c5e29ce516756120be16c34e1176eec7d
S12345-dev <SAMADSHAYAN1010@GMAIL.COM>
2025-03-27 23:33:24 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function myFunction(){
+    var a = 4;
+    document.getElementById("demo").innerHTML = a*a;
+}
+function myFunction1(){
+    var a = 4;
+    document.getElementById("sa").innerHTML = a+a;
+}
+function myFunction2(){
+    var a = 4;
+    document.getElementById("am").innerHTML = a/a;
+}
+function myFunction3(){
+    var a = 4;
+    document.getElementById("d").innerHTML = a-a;
+}
